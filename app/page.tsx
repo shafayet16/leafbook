9f114b9b75404da8e7f515871a93bc1b974139df
شفاعة <hirasan893@gmail.com>
2026-06-17 14:13:31 +0600

    feat: build initial notebook background and textfield

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    // Put this wrapper around your existing main component
+    <div className="w-screen h-screen bg-gradient-to-br from-[#040c08] via-[#0a1e14] to-[#122d1e] flex items-center justify-center p-4">
+      
+      <main className="w-[80vw] h-[80vh] border-2 border-[#593959] m-auto bg-[#161320]">
+        
+        
+      </main>
+
+    </div>
+  );
+}
